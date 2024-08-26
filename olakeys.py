928519a9b807aaa528987b390fcb97fc52e50139
Rahul Kumar <41460419+unitinguncle@users.noreply.github.com>
2024-08-26 16:14:38 +0530

    Update olakeys.py

--- olakeys.py
+++ olakeys.py
@@ -1,4 +1,4 @@
-api_key = "wko8RH3XmUxdfV5cbCHMJdbYTIh7c59dn34xh7kH"
-request_id = "abc"
+api_key = "YOUROLAMAPSKEYHERE"
+request_id = "NOTREQUIRED"
 
-gmap_api_key = "AIzaSyAyROPF0t2DrnW6PnAexBR8cRCO2btWbJk"
+gmap_api_key = "YOURGOOGLEMAPSKEYHERE"
